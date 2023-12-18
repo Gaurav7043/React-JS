968d8557af526453effb981774e9b141d908967e
gaurav <gauravgupta11n@gmail.com>
2023-12-18 15:22:32 +0530

    success

--- vite-project/src/04_Hook/05_Use_Reducer/Use_Reducer_With_PayLoad_Input.jsx
+++ vite-project/src/04_Hook/05_Use_Reducer/Use_Reducer_With_PayLoad_Input.jsx
@@ -1,9 +1,11 @@
 import React, { useReducer, useState } from 'react'
 import { Button, Input } from 'reactstrap'
+import { toast } from "react-toastify"
 
 export default function Use_Reducer_With_PayLoad_Input() {
     const reducer = (state, action)=>{
         if(action.actionType === "INC"){
+            toast.success("Your Data Added Successfully")
             return {count : state.count + action.payLoad}
         }else{
             return state
@@ -31,7 +33,7 @@ export default function Use_Reducer_With_PayLoad_Input() {
                 <div className='mt-3' style={{textAlign: "center"}}>
                     <Input type='text' placeholder='Enter your Number' className='w-25 mb-3 text-center m-auto' value={inpValue} onChange={inpHandler}/>
                     {/* <Button color='danger' onClick={submit}>INC By Number</Button> */}
-                    <Button color='danger' onClick={()=>dispatch({actionType : "INC", payLoad: +inpValue, blank : setInpValue("")})}>INC By Number</Button>
+                    <Button color='danger' onClick={()=>inpValue == "" ? toast.error("Please Fill Data") : dispatch({actionType : "INC", payLoad: +inpValue, blank : setInpValue("")})}>INC By Number</Button>
                 </div>
             </div>
         </>
