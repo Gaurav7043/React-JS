5af5577c5db71dec94097f5520de9149b96b95f3
gaurav <gauravgupta11n@gmail.com>
2023-12-18 22:34:44 +0530

    success

--- vite-project/src/10_Router/Error404.jsx
+++ vite-project/src/10_Router/Error404.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Error404() {
     return (
         <>
-            <h1>Error404</h1>
+            <div className=''>
+                <h1>Error 404</h1>
+                <NavLink to={"/"} className='fs-5 text-black ms-5'>Home</NavLink>
+            </div>
         </>
     )
 }
