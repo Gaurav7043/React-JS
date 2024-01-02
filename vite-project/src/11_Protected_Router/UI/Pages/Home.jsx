989f9965f4dfff856a60967694779683bc4b9d0d
gaurav <gauravgupta11n@gmail.com>
2024-01-02 20:01:59 +0530

    success

--- vite-project/src/11_Protected_Router/UI/Pages/Home.jsx
+++ vite-project/src/11_Protected_Router/UI/Pages/Home.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Button } from 'reactstrap'
+import { Button, Input, Label } from 'reactstrap'
 
 let nameArr = ["gaurav", "harsh", "som", "om"]
 
 export default function Home() {
     const navigate = useNavigate()
+    let [check, setCheck] = useState([])
+
+    const checkHandler = (item)=>{
+        const matchItem = check?.includes(item)
+        if(matchItem){
+            let fliter = check?.filter((e)=> e !== item)
+            setCheck(fliter)
+        }else{
+            setCheck([...check, item])
+        }
+    }
 
     return (
         <>
             <div className='text-center'>
                 <h1>Home</h1>
+                {
+                    check.map((e, i)=>{
+                        return(
+                            <h1 key={i}>{e}</h1>
+                        )
+                    })
+                }
+                <div className='gap-3 d-flex'>
+                    <Input onClick={()=>checkHandler("h1")} type="checkbox" />
+                    <Label check>h1</Label>
+                    <Input onClick={()=>checkHandler("h2")} type="checkbox" />
+                    <Label check>h2</Label>
+                    <Input onClick={()=>checkHandler("h3")} type="checkbox" />
+                    <Label check>h3</Label>
+                </div>
                 <Button className='ms-2 mb-2' color='danger' onClick={()=>navigate(-1)}>Go Back</Button>
             </div>
         </>
