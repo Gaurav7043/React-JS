9b430f1a0f3b012b6d497f4a3a5b89ec68630960
gaurav <gauravgupta11n@gmail.com>
2024-01-03 22:55:14 +0530

    success

--- vite-project/src/11_Protected_Router/UI/Pages/Home.jsx
+++ vite-project/src/11_Protected_Router/UI/Pages/Home.jsx
@@ -4,24 +4,24 @@ import { Button, Input, Label } from 'reactstrap'
 
 
 export default function Home() {
-    const navigate = useNavigate()
-    let [check, setCheck] = useState([])
+    // const navigate = useNavigate()
+    // let [check, setCheck] = useState([])
 
-    const checkHandler = (item)=>{
-        const matchItem = check?.includes(item)
-        if(matchItem){
-            let fliter = check?.filter((e)=> e !== item)
-            setCheck(fliter)
-        }else{
-            setCheck([...check, item])
-        }
-    }
+    // const checkHandler = (item)=>{
+    //     const matchItem = check?.includes(item)
+    //     if(matchItem){
+    //         let fliter = check?.filter((e)=> e !== item)
+    //         setCheck(fliter)
+    //     }else{
+    //         setCheck([...check, item])
+    //     }
+    // }
 
     return (
         <>
             <div className='text-center'>
                 <h1>Home</h1>
-                {
+                {/* {
                     check.map((e, i)=>{
                         return(
                             <h1 key={i}>{e}</h1>
@@ -36,7 +36,7 @@ export default function Home() {
                     <Input checked={check?.includes("h3")} onClick={()=>checkHandler("h3")} type="checkbox" />
                     <Label check>h3</Label>
                 </div>
-                <Button className='ms-2 mb-2' color='danger' onClick={()=>navigate(-1)}>Go Back</Button>
+                <Button className='ms-2 mb-2' color='danger' onClick={()=>navigate(-1)}>Go Back</Button> */}
             </div>
         </>
     )
