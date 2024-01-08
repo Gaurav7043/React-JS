e3717a5db26b04489f5a7a23253855b0cdd1c679
gaurav <gauravgupta11n@gmail.com>
2024-01-09 00:12:48 +0530

    success

--- vite-project/src/08_Input/01_Single_Input/Todo_List.jsx
+++ vite-project/src/08_Input/01_Single_Input/Todo_List.jsx
@@ -7,59 +7,82 @@ export default function Todo_List() {
     let [task, setTask] = useState("")
     let [pendingTask, setPendingTask] = useState([])
     let [doneTask, setDoneTask] = useState([])
-
+    
     // get data from input
     const getData = (ele) => {
         let inputValue = ele?.target?.value
         setTask(inputValue)
     }
 
-    // add task to arry
+    // add task to array
     const addTask = () => {
-        let allData = [...pendingTask, task] // combine old + new data
         if (task.length > 0) {
-            const match = pendingTask.some((e) => {
-                return e === task
-            })
-            if (match) {
-                toast.error("Same Data Already Exists")
+            const matchPending = pendingTask.some((e) => e === task);
+            const matchDone = doneTask.some((e) => e === task);
+
+            if (matchPending || matchDone) {
+                toast.error("Same Data Already Exists");
             } else {
-                setPendingTask(allData)
-                setTask("")
-                localStorage.setItem("task", JSON.stringify([...pendingTask, task]))
-                toast.success("Data Added Successfully")
+                setPendingTask([...pendingTask, task]);
+                setTask("");
+                localStorage.setItem("task", JSON.stringify({ pendingTask: [...pendingTask, task], doneTask }));
+                toast.success("Data Added Successfully");
             }
         } else {
-            toast.error("Please Fill The Input")
+            toast.error("Please Fill The Input");
         }
     }
 
-    // use effect
-    useEffect(() => {
-        let json_data = localStorage.getItem("task")
-        let normal_data = JSON.parse(json_data)
-        setPendingTask(normal_data || [])
-    }, [])
-
     // delete with filter
     const deleteHandler = (index) => {
-        let ans = confirm("Are you Sure ?")
+        let ans = window.confirm("Are you Sure ?");
         if (ans) {
-            let deleteData = doneTask.filter((e, i) => i !== index)
-            setDoneTask(deleteData)
-            localStorage.setItem("task", JSON.stringify(deleteData))
-            toast.info("Data Remove Successfully")
+            let deleteData = doneTask.filter((e, i) => i !== index);
+            setDoneTask(deleteData);
+            localStorage.setItem("task", JSON.stringify({ pendingTask, doneTask: deleteData }));
+            toast.info("Data Remove Successfully");
         } else {
-            toast.warn("You Data Not Remove")
+            toast.warn("Your Data Not Remove");
         }
     }
 
     // single done task
-    const doneTaskHandler = (index)=>{
-        setDoneTask([...doneTask, pendingTask[index]])
-        let newData = pendingTask?.filter((e, i)=> i !== index)
-        setPendingTask(newData)
-    }
+    const doneTaskHandler = (index) => {
+        let done = window.confirm("Your Data Go Done Task");
+        if (done) {
+            setDoneTask([...doneTask, pendingTask[index]]);
+            let newData = pendingTask?.filter((e, i) => i !== index);
+            setPendingTask(newData);
+            localStorage.setItem("task", JSON.stringify({ pendingTask: newData, doneTask: [...doneTask, pendingTask[index]] }));
+            toast.success("Your Data Added Done Task Successfully");
+        } else {
+            toast.warn("Your Data Not Go to done Task");
+        }
+    };
+
+    // single pending task
+    const pendingTaskHandler = (index) => {
+        let pending = window.confirm("Your Data Go Pending Task");
+        if (pending) {
+            setPendingTask([...pendingTask, doneTask[index]]);
+            let newData = doneTask?.filter((e, i) => i !== index);
+            setDoneTask(newData);
+            localStorage.setItem("task", JSON.stringify({ pendingTask: [...pendingTask, doneTask[index]], doneTask: newData }));
+            toast.success("Your Data Added Pending Task Successfully");
+        } else {
+            toast.warn("Your Data Not Go to Pending Task");
+        }
+    };
+
+    // use effect for local storage data save
+    useEffect(() => {
+        let json_data = localStorage.getItem("task");
+        let parsed_data = JSON.parse(json_data);
+        if (parsed_data) {
+            setPendingTask(parsed_data.pendingTask || []);
+            setDoneTask(parsed_data.doneTask || []);
+        }
+    }, []);
 
     return (
         <>
@@ -73,49 +96,64 @@ export default function Todo_List() {
                 </div>
             </div>
 
-            <div className="d-flex gap-4 p-4 justify-content-between align-items-center">
+            <div className="d-flex w-100 p-4 justify-content-between ">
                 {/* pending task */}
-                <div className='w-50 m-auto border dark rounded-3 p-3 mt-5'>
-                    <h1 className='text-center'>Pending Task</h1>
-                    <hr style={{ padding: "5px", backgroundColor: "darkgray" }} />
-                    <ul className='list-inline'>
-                        {
-                            pendingTask.map((e, i) => {
-                                return (
-                                    <div key={i}>
-                                        <div className='d-flex align-items-center justify-content-between'>
-                                            <li className='list-inline-item'>{i + 1}. {e}</li>
-                                            <div className='d-flex gap-2'>
-                                                <CheckCircleFill role='button' color='green' onClick={()=>doneTaskHandler(i)} />
+                <div style={{ minWidth: "45%" }} className="border dark rounded-2 p-2 mt-3">
+                    {
+                        pendingTask.length > 0 ?
+                            <div>
+                                <h1 className="text-center">Pending Task</h1>
+                                <hr style={{ padding: "5px", backgroundColor: "darkgray" }} />
+                                <ul className='list-inline'>
+                                    {pendingTask.map((element, i) => {
+                                        return (
+                                            <div key={i}>
+                                                <div className="d-flex justify-content-between ">
+                                                    <li className='list-inline-item'>{i + 1}. {element}</li>
+                                                    <div className="d-flex gap-2">
+                                                        <CheckCircleFill role="button" color="green" onClick={() => doneTaskHandler(i)} />
+                                                    </div>
+                                                </div>
+                                                <hr />
                                             </div>
-                                        </div>
-                                        <hr />
-                                    </div>
-                                )
-                            })
-                        }
-                    </ul>
+                                        )
+                                    })}
+                                </ul>
+                            </div> :
+                            (
+                                <h1>Please Add Some Pending Data</h1>
+                            )
+                    }
                 </div>
 
                 {/* done task */}
-                <div className='w-50 m-auto border dark rounded-3 p-3 mt-5'>
-                    <h1 className='text-center'>Done Task</h1>
-                    <hr style={{ padding: "5px", backgroundColor: "darkgray" }} />
-                    <ul className='list-inline'>
-                        {
-                            doneTask.map((e, i) => {
-                                return (
-                                    <div key={i}>
-                                        <div className='d-flex align-items-center justify-content-between'>
-                                            <li className='list-inline-item'>{i + 1}. {e}</li>
-                                            <Trash3 color='red' onClick={() => deleteHandler(i)} />
-                                        </div>
-                                        <hr />
-                                    </div>
-                                )
-                            })
-                        }
-                    </ul>
+                <div style={{ minWidth: "45%" }} className="border dark rounded-2 p-2 mt-3">
+                    {
+                        doneTask.length > 0 ?
+                            <div>
+                                <h1 className="text-center">Done Task</h1>
+                                <hr style={{ padding: "5px", backgroundColor: "darkgray" }} />
+                                <ul className='list-inline'>
+                                    {doneTask.map((element, i) => {
+                                        return (
+                                            <div key={i}>
+                                                <div className="d-flex justify-content-between ">
+                                                    <li className='list-inline-item'>{i + 1}. {element}</li>
+                                                    <div className="d-flex gap-2">
+                                                        <CheckCircleFill role="button" color="red" onClick={() => pendingTaskHandler(i)} />
+                                                        <Trash3 onClick={() => deleteHandler(i)} color="red" />
+                                                    </div>
+                                                </div>
+                                                <hr />
+                                            </div>
+                                        )
+                                    })}
+                                </ul>
+                            </div> :
+                            (
+                                <h1>Please Add Some Done Data</h1>
+                            )
+                    }
                 </div>
             </div>
         </>
