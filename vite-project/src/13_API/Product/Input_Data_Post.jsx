8f6646a1142c5aac0fa3487413486eea5600b676
gaurav <gauravgupta11n@gmail.com>
2024-02-04 17:41:32 +0530

    success

--- vite-project/src/13_API/Product/Input_Data_Post.jsx
+++ vite-project/src/13_API/Product/Input_Data_Post.jsx
@@ -270,24 +270,24 @@ export default function Input_Data_Post() {
                                                         {
                                                             e?.size?.map((size, i) => {
                                                                 return (
-                                                                    <div 
-                                                                        key={i} 
+                                                                    <div
+                                                                        key={i}
                                                                         style=
                                                                         {
-                                                                            size == 41 ? 
-                                                                            { 
-                                                                                backgroundColor: "green", 
-                                                                                color: "white", 
-                                                                                border: "1px solid black", 
-                                                                                padding: "0px 5px", 
-                                                                                textAlign: "center" 
-                                                                            } : 
-                                                                            { 
-                                                                                color: "gray", 
-                                                                                border: "1px solid black", 
-                                                                                padding: "0px 5px", 
-                                                                                textAlign: "center" 
-                                                                            }
+                                                                            size == 41 ?
+                                                                                {
+                                                                                    backgroundColor: "green",
+                                                                                    color: "white",
+                                                                                    border: "1px solid black",
+                                                                                    padding: "0px 5px",
+                                                                                    textAlign: "center"
+                                                                                } :
+                                                                                {
+                                                                                    color: "gray",
+                                                                                    border: "1px solid black",
+                                                                                    padding: "0px 5px",
+                                                                                    textAlign: "center"
+                                                                                }
                                                                         }
                                                                     >
                                                                         {size}
