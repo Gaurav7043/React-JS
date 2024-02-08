c8ee000f4a28ca539d5825efd54f0702e2c7e073
gaurav <gauravgupta11n@gmail.com>
2024-02-08 20:33:10 +0530

    success

--- vite-project/src/04_Hook/07_Use_CallBack/DisplayArr.jsx
+++ vite-project/src/04_Hook/07_Use_CallBack/DisplayArr.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function DisplayArr() {
+export default function DisplayArr({ cbFun }) {
+    let [arr, setArr] = useState([])
+
+    useEffect(()=>{
+        let a = cbFun()
+        setArr(a)
+    }, [cbFun])
+
     return (
-        <div>DisplayArr</div>
+        <>
+            <div className='text-center'>
+                <h3>Number = {arr[0]}</h3>
+                <h3>Number + 1 = {arr[1]}</h3>
+                <h3>Number + 2 = {arr[2]}</h3>
+            </div>
+        </>
     )
 }
--- vite-project/src/04_Hook/07_Use_CallBack/UseCallBack.jsx
+++ vite-project/src/04_Hook/07_Use_CallBack/UseCallBack.jsx
@@ -1,33 +1,25 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Button } from 'reactstrap'
+import DisplayArr from './DisplayArr'
 
-function fun(count) {
-    console.log("----generateArr------->");
-    for (let i = 0; i < 1000000000; i++) { }
-    let arr = [count, count + 1, count + 2]
-    return arr
-}
 
 export default function UseCallBack() {
     let [count, setCount] = useState(0)
     let [amount, setAmount] = useState(0)
-    let [arr, setArr] = useState([])
 
     let cbFun = useCallback(() => {
-        return fun(count)
+        return () => {
+            console.log("----generateArr------->");
+            for (let i = 0; i < 1000000000; i++) { }
+            let arr = [count, count + 1, count + 2]
+            return arr
+        }
     }, [count])
-
-    useEffect(() => {
-        let next3NumberArr = cbFun()
-        setArr(next3NumberArr)
-    }, [cbFun])
 
     return (
         <>
             <div className='text-center'>
-                <h3>Number = {arr[0]}</h3>
-                <h3>Number + 1 = {arr[1]}</h3>
-                <h3>Number + 2 = {arr[2]}</h3>
+                <DisplayArr cbFun={cbFun} />
                 <hr className='mt-3 mb-2' style={{ width: "25%", margin: "auto" }} />
                 <h2>Count is {count}</h2>
                 <Button color='danger' onClick={() => setCount(count + 1)}>Inc</Button>
