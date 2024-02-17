0c853f7fb06fa0711cd910221e505d826e053e6d
gaurav <gauravgupta11n@gmail.com>
2024-02-17 21:17:05 +0530

    success

--- vite-project/src/00_Task/ReduxPractice/ReduxMultiSlice/UI/Point/PointUpdate.jsx
+++ vite-project/src/00_Task/ReduxPractice/ReduxMultiSlice/UI/Point/PointUpdate.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { Button } from 'reactstrap'
-import { incPoint } from '../../Redux/Fetures/Point'
+import { Button, Input } from 'reactstrap'
+import { incByInput, incPoint } from '../../Redux/Fetures/Point'
 
 export default function PointUpdate() {
     const dispatch = useDispatch()
+    let [input, setInput] = useState("")
+
+    const addData = () => {
+        dispatch(incByInput(input))
+        setInput("")
+    }
 
     return (
         <>
             <div className='text-center'>
-                <Button color='danger' onClick={()=>dispatch(incPoint())}>Inc</Button>
+                <Button color='danger' onClick={() => dispatch(incPoint())}>Inc</Button>
+            </div>
+            <div className="d-flex align-items-center justify-content-center gap-2 mt-3">
+                <Input value={input} className='w-25' placeholder='Enter Amount' onChange={(e) => setInput(e?.target?.value)} />
+                <Button color='danger' onClick={() => addData()}>Inc By Input</Button>
             </div>
         </>
     )
