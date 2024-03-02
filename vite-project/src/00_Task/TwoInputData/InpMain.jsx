022b682034aef99b68e01ccfbd1825e3bdab7132
gaurav <gauravgupta11n@gmail.com>
2024-03-02 15:25:12 +0530

    success

--- vite-project/src/00_Task/TwoInputData/InpMain.jsx
+++ vite-project/src/00_Task/TwoInputData/InpMain.jsx
@@ -11,9 +11,13 @@ export default function InpMain() {
     let [value, setValue] = useState(initializeData)
 
     return (
-        <>
-            <Inp1 value={value} setValue={setValue} />
-            <Inp2 value={value} setValue={setValue} />
-        </>
+        <div className='d-flex gap-5 border dark p-2 m-2'>
+            <div style={{ flex: "1" }}>
+                <Inp1 value={value} setValue={setValue} />
+            </div>
+            <div style={{ flex: "1" }}>
+                <Inp2 value={value} setValue={setValue} />
+            </div>
+        </div>
     )
 }
