befbcaa76c93874ddc5d4b6e7266748ae7d1f54d
gaurav <gauravgupta11n@gmail.com>
2024-03-05 23:06:17 +0530

    success

--- Mobile-Layers-Project/src/UI/Pages/Collection/ShopNow/ShopNow.jsx
+++ Mobile-Layers-Project/src/UI/Pages/Collection/ShopNow/ShopNow.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { NavLink } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
 import shopnow from '../../../../../public/shopnow-logo.webp'
 
 export default function ShopNow() {
     return (
         <>
-            <NavLink>
+            <NavLink to={"/collection"} role='button'>
                 <img src={shopnow} alt="" style={{height: "auto", maxWidth: "100%"}} />
             </NavLink>
         </>
