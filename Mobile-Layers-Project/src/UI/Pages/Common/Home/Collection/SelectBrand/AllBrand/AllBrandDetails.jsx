448c0f8f70cb137e8bdc21c929620ec748b75989
gaurav <gauravgupta11n@gmail.com>
2024-04-09 14:50:18 +0530

    success

--- Mobile-Layers-Project/src/UI/Pages/Common/Home/Collection/SelectBrand/AllBrand/AllBrandDetails.jsx
+++ Mobile-Layers-Project/src/UI/Pages/Common/Home/Collection/SelectBrand/AllBrand/AllBrandDetails.jsx
@@ -6,6 +6,10 @@ import { faSquareFacebook, faTwitter, faPinterest } from '@fortawesome/free-bran
 import './AllBrand.css'
 import ReviewForm from './ReviewForm'
 import ReviewList from './ReviewList'
+import axios from 'axios'
+import { BE_URL } from '../../../../../../../Config'
+import { toast } from 'react-toastify'
+import { useSelector } from 'react-redux'
 
 export default function AllBrandDetails() {
     let location = useLocation()
@@ -41,6 +45,29 @@ export default function AllBrandDetails() {
     // State to hold the currently selected image
     const [selectedImage, setSelectedImage] = useState(images?.length > 0 ? images[0] : null)
 
+    const data = useSelector((state) => state?.authSlice?.token)
+    // console.log("===data=======>", data)
+
+    // cart
+    const addCardHandler = (id)=>{
+        axios({
+            method: "post",
+            url: BE_URL + `/cart/create/${id}`,
+            headers : {
+                authorization: `Beare ${data}`,
+                "Content-Type": "application/json"
+            }
+        })?.then((res) => {
+            console.log(res?.data?.data)
+            toast.success("success")
+            // setFilter(res?.data?.data)
+        })?.catch((err) => {
+            console.log(err)
+            toast.error("Failed to load product details")
+        })
+        // console.log(id)
+    }
+
     return (
         <>
             <div className='d-flex gap-3 ms-5 ps-4 pt-3 pb-3 collection'>
@@ -65,7 +92,7 @@ export default function AllBrandDetails() {
                     <p className='m-0'>{title}</p>
                     <h1 style={{ letterSpacing: "1px", fontFamily: "sans-serif" }}>{description}</h1>
                     <p style={{ fontSize: "23px" }}>₹ {price - discountPercentage}.00 MRP(inclusive of taxes): <span className='text-decoration-line-through' style={{ color: "gray" }}>₹ {price}.00</span></p>
-                    <Button className='w-100 bg-black rounded-5 mt-4 mb-3'>Add To Cart</Button>
+                    <Button className='w-100 bg-black rounded-5 mt-4 mb-3' onClick={()=>addCardHandler(location?.state?._id)}>Add To Cart</Button>
                     {/* Render images as thumbnails */}
                     <div className='d-flex'>
                         {
